38ee2651e12f0880a02735446829c3c8246a1405
vova_white <belyj.vladimir@gmail.com>
2019-08-01 11:45:30 +0200

    risk done

--- src/app/views/risk/risk.component.ts
+++ src/app/views/risk/risk.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-risk',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk.component.scss']
 })
 export class RiskComponent implements OnInit {
+  params = {
+    x: null,
+    a: 50,
+    b: 50
+  };
 
-  constructor() { }
+  risk: number;
 
-  ngOnInit() {
+  options: Options = {
+    floor: 1,
+    ceil: 100,
+    animate: false,
+    translate: (): string => {
+      return '';
+    }
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  calcRisk() {
+    if (this.params.x) {
+      this.risk = this.params.x * (this.params.a / 100) * (this.params.b / 100);
+    }
   }
-
 }
